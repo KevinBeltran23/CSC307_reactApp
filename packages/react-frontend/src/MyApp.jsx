8482a6a00a9278ab3f381409e85b50c4a1a7aef3
KevinBeltran23 <kevinbeltran2437@gmail.com>
2024-04-12 10:12:05 -0700

    Submit button added and State modification

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,12 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 function MyApp() {
     const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            name: "Dee",
-            job: "Aspiring Actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
+        
     ]);
 
     function removeOneCharacter(index) {
@@ -30,12 +16,17 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function updateList(person) {
+        setCharacters([...characters, person]);
+      }
+
     return (
         <div className="container">
             <Table 
                 characterData={characters}
                 removeCharacter={removeOneCharacter}
             />
+            <Form handleSubmit={updateList} />
         </div>
     );
 }
