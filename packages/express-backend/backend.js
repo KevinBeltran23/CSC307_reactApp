1cf97b3979636b7acc493494642a00790e983936
KevinBeltran23 <kevinbeltran2437@gmail.com>
2024-04-15 14:29:23 -0700

    step 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,14 +3,53 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Fanitor"
+      },
+      {
+        id: "abc123",
+        name: "BIGMAC",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "lilMAC",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
 app.use(express.json());
+
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
 
 app.listen(port, () => {
     console.log(
         'Example app listening at http://localhost:${port}'
     );
 });
+
+
