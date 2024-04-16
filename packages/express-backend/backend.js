41780b8d4758e4f9c50d96aaaa2ef85b41af7a3e
KevinBeltran23 <kevinbeltran2437@gmail.com>
2024-04-15 21:30:27 -0700

    completed assign2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -51,20 +51,48 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserByJob = (job) => {
+    return users["users_list"].filter(
+      (user) => user["job"] === job
+    );
+};
+
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name && user["job"] === job
+    )
+}
+
+const findUserById = (id) => users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+// Find users by name or job
 app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
+    const job = req.query.job;
+
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        result = { users_list: result };
+        res.send(result);
+    } else if (name != undefined) {
         let result = findUserByName(name);
         result = { users_list: result };
+        res.send(result);
+    } else if (job != undefined) {
+        let result = findUserByJob(job);
+        result = { users_list: result};
         res.send(result);
     } else {
         res.send(users);
     }
 });
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
+// Find users by id
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
@@ -75,15 +103,27 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-};
-  
+// Add users
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.send();
 });
+
+// Delete users
+app.delete("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const index = users.users_list.findIndex((user) => user.id === id);
+    if (index !== -1) {
+        // Remove the user from the list
+        users.users_list.splice(index, 1);
+        // Send a success response
+        res.status(200).send(`User with id ${id} has been deleted successfully.`);
+    } else {
+        // User not found
+        res.status(404).send("Resource not found.");
+    }
+});
+
 
 
